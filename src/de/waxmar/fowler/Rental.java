72dc002c13608232c7ffef0ee0b4e3c6e400392d
waxmar <mariowaxenegger@gmail.com>
2014-05-12 11:27:15 +0200

    Extract method

--- src/de/waxmar/fowler/Rental.java
+++ src/de/waxmar/fowler/Rental.java
@@ -37,4 +37,12 @@ class Rental {
 		}
 		return result;
 	}
+
+	public int getFrequentRenterPoints() {
+		// add bonus for a two day new release rental
+		if ((getMovie().getPriceCode() == Movie.NEW_RELEASE)
+				&& getDaysRented() > 1)
+			return 2;
+		return 1;
+	}
 }
